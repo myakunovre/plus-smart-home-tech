b46dfb3700a808091bdd5a276bf4e56f9032d65c
Roman <myakunovre@yandex.ru>
2025-08-19 01:23:57 +0300

    Исправлен pom

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/Collector.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/Collector.java
@@ -2,13 +2,10 @@ package ru.yandex.practicum.telemetry.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = "ru.yandex.practicum.telemetry.collector")
+@SpringBootApplication
 @EnableScheduling
 public class Collector {
 
