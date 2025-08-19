de76eeb68a0f48b876d7ccdbc9e83a7285cfdcc2
Roman <myakunovre@yandex.ru>
2025-08-19 19:48:28 +0300

    Исправлены ошибки

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/Collector.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/Collector.java
@@ -2,22 +2,19 @@ package ru.yandex.practicum.telemetry.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-//@SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 //@ComponentScan(basePackages = "ru.yandex.practicum.telemetry.collector")
-@EnableScheduling
+//@EnableScheduling
 public class Collector {
 
     public static void main(String[] args) {
         SpringApplication.run(Collector.class, args);
     }
 
-    @Scheduled(fixedDelay = 5000)
-    public void taskWithFixedDelay() {
-        System.out.println("Очередное выполнение: " + System.currentTimeMillis() / 1000);
-    }
+//    @Scheduled(fixedDelay = 5000)
+//    public void taskWithFixedDelay() {
+//        System.out.println("Очередное выполнение: " + System.currentTimeMillis() / 1000);
+//    }
 }
